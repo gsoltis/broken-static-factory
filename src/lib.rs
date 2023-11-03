90469e634dffc11d95ad2038fe78f0250f08913b
Greg Soltis <Greg Soltis>
2023-11-03 12:57:27 -0700

    Add a class and a factory

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![deny(clippy::all)]
+use napi::bindgen_prelude::*;
 
 #[macro_use]
 extern crate napi_derive;
@@ -6,4 +7,31 @@ extern crate napi_derive;
 #[napi]
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
+}
+
+async fn always_4() -> i32 {
+  4
+}
+
+#[napi]
+pub async fn future_4() -> i32 {
+  always_4().await
+}
+
+#[napi]
+pub struct MyNumber {
+  pub num: i32
+}
+
+#[napi]
+impl MyNumber {
+  #[napi]
+  pub fn is_4(&self) -> bool {
+    self.num == 4
+  }
+
+  #[napi(factory)]
+  pub fn now_4() -> Self {
+    Self { num: 4 }
+  }
 }
